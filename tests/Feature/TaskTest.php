fd48b4f4d1c871c732cc33ef2cdbfba7de4d1b1e
Idham Adzani <adzani234@gmail.com>
2023-06-06 16:49:29 +0700

    create task test

---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TaskTest extends TestCase {
+  use RefreshDatabase;
+
+  public function test_task_can_be_created() {
+    $task = Task::factory()->create();
+
+    $this->assertInstanceOf(Task::class, $task);
+    $this->assertDatabaseHas('tasks', [
+      'id' => $task->id,
+      'title' => $task->title,
+    ]);
+  }
+
+  public function test_task_can_be_retrieved() {
+    Task::factory(10)->create();
+    $tasks = Task::all();
+
+    $this->assertCount(10, $tasks);
+  }
+
+  public function test_selected_task_can_be_retrieved() {
+    $createdTask = Task::factory()->create();
+    $task = Task::find($createdTask->id);
+
+    $this->assertEquals($createdTask->title, $task->title);
+    $this->assertEquals($createdTask->description, $task->description);
+  }
+
+  public function test_selected_task_can_be_updated() {
+    $createdTask = Task::factory()->create();
+    $selectedTask = Task::find($createdTask->id);
+    $selectedTask->title = 'New Title';
+    $selectedTask->save();
+
+    $this->assertDatabaseHas('tasks', [
+      'title' => 'New Title'
+    ]);
+  }
+
+  public function test_selected_task_can_be_deleted() {
+    $createdTask = Task::factory()->create();
+    Task::destroy($createdTask->id);
+
+    $this->assertDatabaseMissing('tasks', [
+      'id' => $createdTask->id,
+    ]);
+  }
+}
