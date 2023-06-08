8a6a2c40c5acd6646aea7887f2546429d5c48fef
Idham Adzani <adzani234@gmail.com>
2023-06-08 16:36:18 +0700

    create task.blade file

---
+++ resources/views/task.blade.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+  <title>Task List</title>
+</head>
+
+<body>
+  <table border="1">
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>user id</th>
+        <th>title</th>
+        <th>desc</th>
+        <th>due date</th>
+        <th>priority</th>
+        <th>status</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($tasks as $task)
+      <tr>
+        <td>{{$task->id}}</td>
+        <td>{{$task->user_id}}</td>
+        <td>{{$task->title}}</td>
+        <td>{{$task->description}}</td>
+        <td>{{$task->due_date}}</td>
+        <td>{{$task->priority}}</td>
+        <td>{{$task->status}}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+</body>
+
+</html>
